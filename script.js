908c8ae090409ba3c50ea7992bd70c819dede43a
dakshdetwal-ai <dakshdetwal10@gmail.com>
2026-09-19 14:24:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("DETwal Trading Platform loaded.");
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("DETwal button clicked");
+    });
+});
